7080073c7c80f44fa8457f11d30e683d7a8f62c4
Ryan Owens <RyanOwens@linux.com>
2016-05-19 15:12:47 -0500

    Missing composer autload features

--- app/FTPUtil.php
+++ app/FTPUtil.php
@@ -1,22 +1,24 @@
-<?
+<?php
 use App\Project;
-use Storage;
 // May need to find where Storage is located in the project
-public function createprojectfile(Project $project)
+function createprojectfile(Project $project)
 {
     $date = new DateTime;
-    $filename = $project->get('project_name') . '-' . $date->format('mdY');
-    $content = $project->get('project_name');
+    $filename = $project['project_name'] . '-' . $date->format('mdY') . '.txt';
+    $content = $project['project_name'];
 
-    Storage::disk('local')->put($filename, $contents);
+    Storage::disk('local')->put($filename, $content);
 
     return $filename;
 }
 
-public function upload(Project $project, $filename)
+function upload(Project $project, $filename)
 {
-    $conn_id = ftp_connect($project->get('storage_server_url'));
-    $login_result = ftp_login($conn_id,$project->get('storage_server_username'),$project->get('storage_server_password'));
+    $conn_id = ftp_connect($project['storage_server_url']);
+    $login_result = ftp_login($conn_id,
+        $project['storage_server_username'],
+        $project['storage_server_password']
+      );
 
     // Send the file
     if(ftp_put($conn_id, $filename, 'storage/app' . $filename, FTP_ASCII))
