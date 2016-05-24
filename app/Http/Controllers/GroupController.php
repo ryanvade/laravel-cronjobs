2105a2908b821fd5cc9681eaa402a4a89a8f9994
Ryan Owens <RyanOwens@linux.com>
2016-05-24 14:32:33 -0500

    Show groups in groupcontroller is working

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Gate;
 use App\Group;
 use App\User;
 use App\Project;
@@ -14,33 +15,53 @@ class GroupController extends Controller
     public function show(Request $request)
     {
         $user = Auth::user();
-        $group = $user->group_id;
+        $group = Group::find($user->group_id);
         $project = Project::find($group->project_id);
+        $admin = User::find($group->project_admin_id);
+
         if(Gate::denies('view-group', $group))
         {
-          $admin = User::find($group->project_admin_id);
-          return view('unathorized', [
+            return view('unathorized', [
             'admin_name' => $admin->name,
             'admin_email' => $admin->email,
           ]);
         }else {
           if(Gate::denies('edit-group', $group))
           {
-            
+            return view('group.user-layout', [
+              'project_name' => $project->project_name,
+              'admin_email' =>  $admin->email,
+              'admin_name' => $admin->name,
+            ]);
           }else {
-
+            $users = User::where('group_id',  $group->id)->get();
+              return view('group.admin-layout', [
+                'project_name' => $project->project_name,
+                'users' => $users,
+              ]);
           }
         }
     }
 
-    public function update(Request $request)
+    public function edit(Request $request)
     {
+      $user = Auth::user();
+      $group = Group::find($user->group_id);
+      $project = Project::find($group->project_id);
 
+      if(Gate::denies('edit-group', $group))
+      {
+        return view('unathorized', [
+        'admin_name' => $admin->name,
+        'admin_email' => $admin->email,
+      ]);
+      }else {
+        $users = User::where('group_id',  $group->id)->get();
+          return view('group.admin-layout', [
+            'project_name' => $project->project_name,
+            'users' => $users,
+          ]);
+      }
     }
 
-    public function remove(Request $request)
-    {
-
-
-    }
 }
